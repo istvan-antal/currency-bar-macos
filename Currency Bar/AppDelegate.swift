172f3b5c91ead1dfd751aac8467e8e86b83995f7
István Antal <istvan.m.antal@gmail.com>
2017-02-24 22:44:38 +0000

    Initial Commit

---
+++ Currency Bar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Currency Bar
+//
+//  Created by Antal István on 24/02/2017.
+//  Copyright © 2017 Antal István. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Currency Bar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Currency Bar
+//
+//  Created by Antal István on 24/02/2017.
+//  Copyright © 2017 Antal István. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
