068310bb906d85060769edc3815731c840841a8c
István Antal <istvan.m.antal@gmail.com>
2017-02-26 17:26:05 +0000

    Cleaned up code

--- Currency Bar/AppDelegate.swift
+++ Currency Bar/AppDelegate.swift
@@ -16,14 +16,23 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     var lastUpdatedTime: Date?
     let menu = NSMenu()
     let lastUpdateIndicator = NSMenuItem()
+    let currencyPair = "gbphuf"
+    let detailsUrl: String
+    let dataUrl: String
+    let request: URLRequest
     
     override init() {
+        dataUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fd%2Fquotes.csv%3Fe%3D.csv%26f%3Dnl1d1t1%26s%3D\(currencyPair)%3DX%22%3B&format=json&callback="
+        detailsUrl = String(format: "https://uk.finance.yahoo.com/quote/%@=X?p=%@=X", currencyPair, currencyPair)
+        request = URLRequest(url: URL(string: dataUrl)!)
+        
         super.init()
+        
         menu.delegate = self
         
         statusItem.title = "..."
-        
         lastUpdateIndicator.title = "Updated: never"
+        
         menu.addItem(lastUpdateIndicator)
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Open Chart", action: #selector(openRatesPage(sender:)), keyEquivalent: "P"))
@@ -36,6 +45,34 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     public func menuWillOpen(_ menu: NSMenu) {
         lastUpdateIndicator.title = String(describing: Int(floor(-(lastUpdatedTime?.timeIntervalSinceNow)!))) + " seconds ago"
     }
+    
+    @IBAction func openRatesPage(sender: AnyObject) {
+        NSWorkspace.shared().open(URL(string: detailsUrl)!)
+    }
+    
+    @IBAction func exitNow(sender: AnyObject) {
+        NSApplication.shared().terminate(self)
+    }
+    
+    func performUpdate() {
+        URLSession.shared.dataTask(with: request) {data, response, err in
+            do {
+                guard let result = try JSONSerialization.jsonObject(with: data!) as? [String: [String: Any]] else {
+                    print("error trying to convert data to JSON")
+                    return
+                }
+                let results = result["query"]?["results"] as! [String: [String: Any]]
+                let rate = String(describing: results["row"]!["col1"]!)
+                self.statusItem.title = rate
+                self.lastUpdatedTime = Date()
+                self.lastUpdateIndicator.title = "Updated: " + (self.lastUpdatedTime?.description)!
+                print("Update complete")
+            } catch {
+                print("error trying to convert data to JSON")
+                return
+            }
+        }.resume()
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -44,38 +81,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         let queue = DispatchQueue(label: "xyz.istvan.timer")  // you can also use `DispatchQueue.main`, if you want
         timer = DispatchSource.makeTimerSource(queue: queue)
         timer!.scheduleRepeating(deadline: .now(), interval: .seconds(60))
-        timer!.setEventHandler { [weak self] in
-            var request = URLRequest(url: URL(string: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fd%2Fquotes.csv%3Fe%3D.csv%26f%3Dnl1d1t1%26s%3Dgbphuf%3DX%22%3B&format=json&callback=")!)
-            request.httpMethod = "POST"
-            let session = URLSession.shared
-            
-            session.dataTask(with: request) {data, response, err in
-                do {
-                    guard let result = try JSONSerialization.jsonObject(with: data!) as? [String: [String: Any]] else {
-                        print("error trying to convert data to JSON")
-                        return
-                    }
-                    let results = result["query"]?["results"] as! [String: [String: Any]]
-                    let rate = String(describing: results["row"]!["col1"]!)
-                    self?.statusItem.title = rate
-                    self?.lastUpdatedTime = Date()
-                    self?.lastUpdateIndicator.title = "Updated: " + (self?.lastUpdatedTime?.description)!
-                    print("Update complete")
-                } catch {
-                    print("error trying to convert data to JSON")
-                    return
-                }
-                }.resume()
-        }
+        timer!.setEventHandler(handler: self.performUpdate)
         timer!.resume()
-    }
-    
-    @IBAction func openRatesPage(sender: AnyObject) {
-        NSWorkspace.shared().open(URL(string: "https://uk.finance.yahoo.com/quote/GBPHUF=X?p=GBPHUF=X")!)
-    }
-    
-    @IBAction func exitNow(sender: AnyObject) {
-        NSApplication.shared().terminate(self)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
